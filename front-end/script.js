d010fe5f7f308996aaa5b3aa9ca92f0098997619
davi <davimelovas61@gmail.com>
2025-05-15 08:40:41 -0300

    Checkpoint: Vou começar a modificar a sidebar esquerda

--- front-end/script.js
+++ front-end/script.js
@@ -1,42 +1,12 @@
-function esconderSideBar(sidebarId, menuId) {
+function esconderSideBar(sidebarID) {
     console.log("FUNCAO CHAMADA")
-    const sidebar = document.getElementById(sidebarId);
-    let menu = document.getElementById(menuId);
-    if (!sidebar || !menu) {
-        // console.warn('Sidebar não encontrada.');
-        // return;
-        if (!sidebar)
-        {
-            console.warn('Sidebar não encontrada');
-            return;
-        }
-        else if(!menu)
-        {
-            menu = document.getElementById("menu-sanduiche-escondido");
-
-            if(!menu)
-            {
-                console.warn('Menu não encontradao');
-                return;
-            }
-        }
-    }
-
-    sidebar.classList.toggle("sidebar-escondida");
-
-    if(menu.id == "menu-sanduiche") menu.id = "menu-sanduiche-escondido"; 
-    else if(menu.id == "menu-sanduiche-escondido") menu.id = "menu-sanduiche";
-
-}   
-
-function esconderSideBar2(sidebarID) {
-    console.log("FUNCAO CHAMADA")
+    
     const sidebar = document.getElementById(sidebarID);
-    // let menu = document.getElementsByClassName(menuId);
-    if (!sidebar) {
+    
+    if (!sidebar){
         console.warn('Sidebar não encontrada.');
         return;
     }
-
+    
     sidebar.classList.toggle("sidebar-escondida");
 }   
